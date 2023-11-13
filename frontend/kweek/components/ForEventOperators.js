81a3dadfd18a094710de719e3aff7d5dd5e9ec8d
DahunLee <cross1731@gmail.com>
2023-11-13 12:52:18 +0900

    eddit details

--- frontend/kweek/components/ForEventOperators.js
+++ frontend/kweek/components/ForEventOperators.js
@@ -156,7 +156,7 @@ export default function ForEventOperators(props) {
             </TestimonialContent>
             <TestimonialAvatar
               src={
-                ''
+                '/images/songchan.png'
               }
               name={'LEE SEONGCHAN'}
               title={props.currentLang=='en'?
